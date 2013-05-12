7d90cf142de9896aea5285762509701431fc946d
Anil Bridgpal <bridgpal@gmail.com>
2013-05-11 18:30:14 -0700

    foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BikeOn::Application.config.secret_token = '94d1b135096eb55d3eb19bcfaed451abad1410eef9c8ed26b4877a9e8fa68e8c7308ce83e122940f3992d9ea8cfe7875dc9c99e48a718b763667f9917bcc3401'
