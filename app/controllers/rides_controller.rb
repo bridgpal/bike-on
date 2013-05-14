57e228e62888995b29760ea8763dc9a7886ed9eb
Anil Bridgpal <bridgpal@gmail.com>
2013-05-13 22:53:49 -0700

    added route detail

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -12,14 +12,10 @@ include RidesHelper
   def show
     query = Parse::Query.new("Ride")
     query.eq("objectId", params[:id])
-    ride = query.get.first
-    data = JSON.parse(open(ride["data"].url ).read)
+    @ride = query.get.first
+    data = JSON.parse(open(@ride["data"].url ).read)
 
-    @max_speed =  data.map {|d| d["speed"]}.max
-    altitude = data.map {|d| d["altitude"]}
-    @altitude_change = altitude.max - altitude.min
-
-    @morris_data = data.map {|d| { time: format_time(d["timestamp"]), speed: meters_to_miles(d["speed"]), altitude: meters_to_feet(d["altitude"]) } }.each_slice(2).map(&:first)
+    @morris_data = data.map {|d| { time: format_time(d["timestamp"]), speed: meters_to_miles(d["speed"]), altitude: meters_to_feet(d["altitude"]) } }
     @leaflet_data = data.map {|d|  [d["lat"], d["long"]] }
   end
 
