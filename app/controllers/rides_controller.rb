62ae1d10a4b17628c4a3322cc2ba2849666b4ea2
Anil Bridgpal <bridgpal@gmail.com>
2013-05-16 14:48:50 -0700

    json api

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -21,16 +21,26 @@ include RidesHelper
     data = JSON.parse(open(@ride["data"].url ).read)
     @morris_data = data.map {|d| { time: format_time(d["timestamp"]), speed: meters_to_miles(d["speed"]), altitude: meters_to_feet(d["altitude"]) } }
     @leaflet_data = data.map {|d|  [d["lat"], d["long"]] }
+    respond_to do |format|
+      format.html
+      format.json do
+
+          @d = data.map {|d| { title: format_time(d["timestamp"]), value: meters_to_miles(d["speed"])} }
+          @panic = { graph: { title: "Ride Data", datasequences: [{title: "Speed", type: "line", datapoints: @d}] }}
+          render :json => @panic
+      end
+    end
+
   end
 
 
   def speed
+
+    #refactor this
     query = Parse::Query.new("Ride")
     query.eq("objectId", params[:id])
     @ride = query.get.first
     data = JSON.parse(open(@ride["data"].url ).read)
-
-
     @morris_data = data.map {|d| { time: format_time(d["timestamp"]), speed: meters_to_miles(d["speed"]), altitude: meters_to_feet(d["altitude"]) } }
   end
 end
