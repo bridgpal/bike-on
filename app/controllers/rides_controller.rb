76e41abc59e11761fb30c30ee1abe836062c6676
Anil Bridgpal <bridgpal@gmail.com>
2013-05-15 23:20:06 -0700

    max values

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -8,23 +8,29 @@ include RidesHelper
     query.order = :descending
     query.include = "creator"
     @rides = query.get
+    @max_speed = @rides.map{|r| r["maxSpeed"]}.max.round(2).to_s + " mph"
+    @max_distance = meters_in_miles(@rides.map{|r| r["distance"]}.max.round(2)).to_s + " miles"
+
   end
+
 
   def show
     query = Parse::Query.new("Ride")
     query.eq("objectId", params[:id])
     @ride = query.get.first
     data = JSON.parse(open(@ride["data"].url ).read)
-
     @morris_data = data.map {|d| { time: format_time(d["timestamp"]), speed: meters_to_miles(d["speed"]), altitude: meters_to_feet(d["altitude"]) } }
     @leaflet_data = data.map {|d|  [d["lat"], d["long"]] }
   end
+
 
   def speed
     query = Parse::Query.new("Ride")
     query.eq("objectId", params[:id])
     @ride = query.get.first
     data = JSON.parse(open(@ride["data"].url ).read)
+
+
     @morris_data = data.map {|d| { time: format_time(d["timestamp"]), speed: meters_to_miles(d["speed"]), altitude: meters_to_feet(d["altitude"]) } }
   end
 end
